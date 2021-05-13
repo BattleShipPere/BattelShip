d7341a4e15524c57e434c24b6780844d2503e486
skater92 <83410216+skater92@users.noreply.github.com>
2021-05-13 17:39:17 +0200

    Boton quitado

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,24 +29,14 @@ namespace BattelShip
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(172, 85);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(349, 179);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "PROBANDO (NO LO ABRAIS EN CODIGO)!!";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(681, 529);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,8 +44,6 @@ namespace BattelShip
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
