1103a5cdf1789de2c9a9b5392ff5def2e2f3a35a
Alvaro Martinez Gutierrez <alvaromagu2002@gmail.com>
2021-05-18 16:25:42 +0200

    Drag and Drop primeros pasos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,14 +30,14 @@ namespace BattelShip
         private void InitializeComponent()
         {
             this.table_tablero = new System.Windows.Forms.TableLayoutPanel();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pic_Submarine = new System.Windows.Forms.PictureBox();
+            this.pic_Patrol = new System.Windows.Forms.PictureBox();
+            this.pic_Destroyer = new System.Windows.Forms.PictureBox();
+            this.pic_Carrier = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Submarine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Patrol)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Destroyer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Carrier)).BeginInit();
             this.SuspendLayout();
             // 
             // table_tablero
@@ -45,7 +45,7 @@ namespace BattelShip
             this.table_tablero.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.table_tablero.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.table_tablero.BackColor = System.Drawing.Color.Beige;
             this.table_tablero.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
             this.table_tablero.ColumnCount = 10;
             this.table_tablero.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
@@ -75,51 +75,59 @@ namespace BattelShip
             this.table_tablero.Size = new System.Drawing.Size(500, 500);
             this.table_tablero.TabIndex = 0;
             // 
-            // pictureBox4
+            // pic_Submarine
             // 
-            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox4.Location = new System.Drawing.Point(622, 144);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(150, 50);
-            this.pictureBox4.TabIndex = 4;
-            this.pictureBox4.TabStop = false;
+            this.pic_Submarine.BackgroundImage = global::BattelShip.Properties.Resources.submarine;
+            this.pic_Submarine.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_Submarine.Location = new System.Drawing.Point(622, 144);
+            this.pic_Submarine.Name = "pic_Submarine";
+            this.pic_Submarine.Size = new System.Drawing.Size(150, 50);
+            this.pic_Submarine.TabIndex = 4;
+            this.pic_Submarine.TabStop = false;
+            this.pic_Submarine.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pic_Barcos_MouseDown);
             // 
-            // pictureBox3
+            // pic_Patrol
             // 
-            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox3.Location = new System.Drawing.Point(722, 256);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
+            this.pic_Patrol.BackgroundImage = global::BattelShip.Properties.Resources.patrol;
+            this.pic_Patrol.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_Patrol.Location = new System.Drawing.Point(722, 256);
+            this.pic_Patrol.Name = "pic_Patrol";
+            this.pic_Patrol.Size = new System.Drawing.Size(50, 50);
+            this.pic_Patrol.TabIndex = 3;
+            this.pic_Patrol.TabStop = false;
+            this.pic_Patrol.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pic_Barcos_MouseDown);
             // 
-            // pictureBox2
+            // pic_Destroyer
             // 
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(672, 200);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
+            this.pic_Destroyer.BackgroundImage = global::BattelShip.Properties.Resources.destroyer;
+            this.pic_Destroyer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_Destroyer.Location = new System.Drawing.Point(672, 200);
+            this.pic_Destroyer.Name = "pic_Destroyer";
+            this.pic_Destroyer.Size = new System.Drawing.Size(100, 50);
+            this.pic_Destroyer.TabIndex = 2;
+            this.pic_Destroyer.TabStop = false;
+            this.pic_Destroyer.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pic_Barcos_MouseDown);
             // 
-            // pictureBox1
+            // pic_Carrier
             // 
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(572, 88);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(200, 50);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.pic_Carrier.BackgroundImage = global::BattelShip.Properties.Resources.carrier;
+            this.pic_Carrier.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_Carrier.Location = new System.Drawing.Point(572, 88);
+            this.pic_Carrier.Name = "pic_Carrier";
+            this.pic_Carrier.Size = new System.Drawing.Size(200, 50);
+            this.pic_Carrier.TabIndex = 1;
+            this.pic_Carrier.TabStop = false;
+            this.pic_Carrier.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pic_Barcos_MouseDown);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pic_Submarine);
+            this.Controls.Add(this.pic_Patrol);
+            this.Controls.Add(this.pic_Destroyer);
+            this.Controls.Add(this.pic_Carrier);
             this.Controls.Add(this.table_tablero);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
             this.Margin = new System.Windows.Forms.Padding(2);
@@ -127,10 +135,10 @@ namespace BattelShip
             this.Name = "Form1";
             this.Text = "Form1";
             this.Resize += new System.EventHandler(this.Form1_Resize);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Submarine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Patrol)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Destroyer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Carrier)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -138,10 +146,10 @@ namespace BattelShip
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel table_tablero;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pic_Carrier;
+        private System.Windows.Forms.PictureBox pic_Destroyer;
+        private System.Windows.Forms.PictureBox pic_Patrol;
+        private System.Windows.Forms.PictureBox pic_Submarine;
     }
 }
 
